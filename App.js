a5a2f5449ea5f81d7c1eb1443413d364325be00a
Macmini <himanshusinha@himanshus-Mini.bbrouter>
2023-10-07 21:48:53 +0530

    latest

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppNavigation from './src/navigations/AppNavigation';
+const App = () => {
+  return <AppNavigation />;
+};
+
+export default App;
---
+++ src/navigations/AppNavigation.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppNavigation;
